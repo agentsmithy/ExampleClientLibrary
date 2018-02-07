9655d4e1cdb1b9e2d970cbf52273b43f8777026e
James Reeve <jreeve@gmail.com>
2018-02-08 07:15:29 +1100

    Initial Commit

---
+++ ExampleClientLibrary/ExampleClientLibrary.h
@@ -0,0 +1,19 @@
+//
+//  ExampleClientLibrary.h
+//  ExampleClientLibrary
+//
+//  Created by James Reeve on 8/2/18.
+//  Copyright © 2018 James Reeve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleClientLibrary.
+FOUNDATION_EXPORT double ExampleClientLibraryVersionNumber;
+
+//! Project version string for ExampleClientLibrary.
+FOUNDATION_EXPORT const unsigned char ExampleClientLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleClientLibrary/PublicHeader.h>
+
+
